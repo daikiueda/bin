8a192df80d0bd4a8ff8afb630a68ade54b9b83ed
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2015-03-18 04:41:33 +0900

    first commit

---
+++ init-node-project.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+#----------------------------------------
+# init-node-poject 
+#----------------------------------------
+#
+# fetch from https://github.com/daikiueda/node-module-scaffold/
+
+curl -sL https://github.com/daikiueda/node-module-scaffold/tarball/master | tar xz --strip=1
+
