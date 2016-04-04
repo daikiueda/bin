6c1f5e4188da8ce636d758efbd893ef29ae0284e
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2016-04-05 01:00:28 +0900

    add check-subdir-git-status.sh

---
+++ check-subdir-git-status.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+for dirname in $(ls -F | grep /); do
+    ls "$dirname.git" 1> /dev/null 2>&1
+    if [ $? -eq 0 ]
+    then
+        echo $dirname
+        cd $dirname
+        git status -sb
+        cd ..
+    fi
+done;
